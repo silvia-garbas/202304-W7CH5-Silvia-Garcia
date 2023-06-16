517f564f2662dd77ccb386408a3168905f1890dd
Silvia <silvia.garbas@gmail.com>
2023-06-16 22:44:25 +0200

    Solved error compilation

--- src/front/index.js
+++ src/front/index.js
@@ -1,65 +1,62 @@
-// "use strict";
-// /* eslint-disable padding-line-between-statements */
+/* eslint-disable padding-line-between-statements */
 // const url = 'http://localhost:4400';
 // const storeName = 'Sample';
 // function main() {
-//     console.log('Loaded');
-//     let state = {};
-//     const formElement = document.querySelector('.login-form');
-//     const logoutElement = document.querySelector('.logout');
-//     const buttonElement = document.querySelector('show-button');
-//     const store = localStorage.getItem(storeName);
-//     if (store) {
-//         state.token = JSON.parse(store).token;
-//         formElement?.setAttribute('hidden', 'true');
-//         logoutElement?.removeAttribute('hidden');
-//     }
-//     else {
-//         formElement?.removeAttribute('hidden');
-//         logoutElement?.setAttribute('hidden', 'true');
-//     }
-//     const login = async (event) => {
-//         event.preventDefault();
-//         const { elements } = event.target;
-//         const data = {
-//             user: elements.namedItem('user').value,
-//             passwd: elements.namedItem('passwd').value,
-//         };
-//         const urlLogin = url + '/user/login';
-//         const response = await fetch(urlLogin, {
-//             method: 'PATCH',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify(data),
-//         });
-//         state = await response.json();
-//         localStorage.setItem(storeName, JSON.stringify({ token: state.token }));
-//         formElement?.setAttribute('hidden', 'true');
-//         logoutElement?.removeAttribute('hidden');
-//         console.log(state);
+//   console.log('Loaded');
+//   let state: any = {};
+//   const formElement = document.querySelector('.login-form');
+//   const logoutElement = document.querySelector('.logout');
+//   const buttonElement = document.querySelector('show-button');
+//   const store = localStorage.getItem(storeName);
+//   if (store) {
+//     state.token = JSON.parse(store).token;
+//     formElement?.setAttribute('hidden', 'true');
+//     logoutElement?.removeAttribute('hidden');
+//   } else {
+//     formElement?.removeAttribute('hidden');
+//     logoutElement?.setAttribute('hidden', 'true');
+//   }
+//   const login = async (event: Event) => {
+//     event.preventDefault();
+//     const { elements } = event.target! as HTMLFormElement;
+//     const data = {
+//       user: (elements.namedItem('user') as HTMLFormElement).value,
+//       passwd: (elements.namedItem('passwd') as HTMLFormElement).value,
 //     };
-//     const logout = () => {
-//         localStorage.removeItem(storeName);
-//         state = {};
-//         formElement?.removeAttribute('hidden');
-//         logoutElement?.setAttribute('hidden', 'true');
-//     };
-//     const handleClick = async () => {
-//         // eslint-disable-next-line no-useless-return
-//         if (!state.token)
-//             return;
-//         const urlFilms = url + '/film';
-//         const response = await fetch(urlFilms, {
-//             headers: {
-//                 Authorization: 'Bearer ' + state.token, // Este espacio muy importante
-//             },
-//         });
-//         const result = await response.json();
-//         console.log(result);
-//     };
-//     formElement.addEventListener('submit', login);
-//     buttonElement?.addEventListener('click', handleClick);
-//     logoutElement?.addEventListener('click', logout);
+//     const urlLogin = url + '/user/login';
+//     const response = await fetch(urlLogin, {
+//       method: 'PATCH',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(data),
+//     });
+//     state = await response.json();
+//     localStorage.setItem(storeName, JSON.stringify({ token: state.token }));
+//     formElement?.setAttribute('hidden', 'true');
+//     logoutElement?.removeAttribute('hidden');
+//     console.log(state);
+//   };
+//   const logout = () => {
+//     localStorage.removeItem(storeName);
+//     state = {};
+//     formElement?.removeAttribute('hidden');
+//     logoutElement?.setAttribute('hidden', 'true');
+//   };
+//   const handleClick = async () => {
+//     // eslint-disable-next-line no-useless-return
+//     if (!state.token) return;
+//     const urlFilms = url + '/film';
+//     const response = await fetch(urlFilms, {
+//       headers: {
+//         Authorization: 'Bearer ' + state.token,// Este espacio muy importante
+//       },
+//     });
+//     const result = await response.json();
+//     console.log(result);
+//   };
+//   formElement!.addEventListener('submit', login);
+//   buttonElement?.addEventListener('click', handleClick);
+//   logoutElement?.addEventListener('click', logout);
 // }
 // document.addEventListener('DOMContentLoaded', main);
